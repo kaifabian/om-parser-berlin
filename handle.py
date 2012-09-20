729eb998b46a808054b46aee6a42111da4591fd8
Kai Fabian <kai.fabian@student.hpi.uni-potsdam.de>
2012-09-20 16:01:17 +0200

    Switching to 4-space indent

--- handle.py
+++ handle.py
@@ -7,45 +7,45 @@ from mensa import scrape_mensa, meta_names
 
 uri = "/index.xml"
 if 'REQUEST_URI' in os.environ:
-	uri = os.environ['REQUEST_URI']
+    uri = os.environ['REQUEST_URI']
 
 mensa = None
 
 if uri.endswith(".xml"):
-	filename = os.path.basename(uri)
-	mensa = filename[:-4]
-	if not mensa in meta_names:
-		mensa = None
+    filename = os.path.basename(uri)
+    mensa = filename[:-4]
+    if not mensa in meta_names:
+        mensa = None
 
 if mensa:
-	try:
-		data = scrape_mensa(mensa)
-		print "Content-Type: application/xml; charset=utf-8"
-		print "Content-Length: "+str(len(data))
-		print ""
-		print data
-	except Exception, e:
-		print "Status: 404 Not Found"
-		print "Content-Type: application/xml; charset=utf-8"
-		print ""
-		print """<?xml version="1.0" encoding="UTF-8"?>"""
-		print "<error>"
-		print " <code>404</code>"
-		print " <message>Mensa not found</message>"
-		print " <debug-data>" + repr(e) + "</debug-data>"
-		print "</error>"
+    try:
+        data = scrape_mensa(mensa)
+        print "Content-Type: application/xml; charset=utf-8"
+        print "Content-Length: "+str(len(data))
+        print ""
+        print data
+    except Exception, e:
+        print "Status: 404 Not Found"
+        print "Content-Type: application/xml; charset=utf-8"
+        print ""
+        print """<?xml version="1.0" encoding="UTF-8"?>"""
+        print "<error>"
+        print " <code>404</code>"
+        print " <message>Mensa not found</message>"
+        print " <debug-data>" + repr(e) + "</debug-data>"
+        print "</error>"
 
 else:
-	print "Status: 404 Not Found"
-	print "Content-Type: application/xml; charset=utf-8"
-	print ""
-	print """<?xml version="1.0" encoding="UTF-8"?>"""
-	print "<error>"
-	print " <code>404</code>"
-	print " <message>Mensa not found</message>"
-	print " <debug-data>"
-	print "  <list-desc>Valid filenames</list-desc>"
-	for mensa in meta_names:
-		print "  <list-item>" + mensa + ".xml</list-item>"
-	print " </debug-data>"
-	print "</error>"
+    print "Status: 404 Not Found"
+    print "Content-Type: application/xml; charset=utf-8"
+    print ""
+    print """<?xml version="1.0" encoding="UTF-8"?>"""
+    print "<error>"
+    print " <code>404</code>"
+    print " <message>Mensa not found</message>"
+    print " <debug-data>"
+    print "  <list-desc>Valid filenames</list-desc>"
+    for mensa in meta_names:
+        print "  <list-item>" + mensa + ".xml</list-item>"
+    print " </debug-data>"
+    print "</error>"
